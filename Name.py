3b748c9c70739e32576f1d9237a1e88673295c72
Christine Arais <araischristine@gmail.com>
2024-11-13 14:44:55 +0400

    Name.py
    usage of concatenation

---
+++ Name.py
@@ -0,0 +1,6 @@
+FirstName = (input ('Input your first name:'))
+SecondName = (input ('Input your second name:'))
+print(f'Your full name is {FirstName + SecondName}.')
+
+name = (input ("Enter your name: "))
+print (name)
